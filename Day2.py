41c613d98fd293286791e5f748967b926ca808ee
Giang Le Hoang <gianglehoang14205@gmail.com>
2024-11-12 16:05:35 +0700

    python

--- Day2.py
+++ Day2.py
@@ -11,11 +11,11 @@ else:
 a=int(input("Nhap diem tong ket mon tieng anh:"))
 if a>=8.5:
     print("Xep loai: A")
-elif 7<=a<=8.49:
+elif 7<=a:
     print("Xep loai: B")
-elif 5<=a<=6.99:
+elif 5<=a:
     print("Xep loai: C")
-elif 4<=a<=4.99:
+elif 4<=a:
     print("Xep loai: D")
 else:
     print("Xep loai: F =>> Ban danh mat 3.112.500")
